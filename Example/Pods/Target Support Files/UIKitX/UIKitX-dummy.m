87aaad749c6e88c8566a72e1e0caf08104aa4685
Geonu Jeon <geonu1109@daou.co.kr>
2020-01-08 16:11:09 +0900

    initial commit

---
+++ Example/Pods/Target Support Files/UIKitX/UIKitX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIKitX : NSObject
+@end
+@implementation PodsDummy_UIKitX
+@end
--- Example/UIKitX/ViewController.swift
+++ Example/UIKitX/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import UIKitX
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstView: UIView = UIView(frame: .zero)
+            .withSuperview(self.view)
+            .withBackgroundColor(.orange)
+            .withCornerRadius(5)
+            .withPositionConstraint(to: .top, offset: 20)
+            .withPositionConstraint(to: .centerX)
+            .withSizeConstraint(.width, value: 80)
+            .withSizeConstraint(.height, value: 30)
+        
+        let secondView: UIView = UIView(frame: .zero)
+            .withSuperview(self.view)
+            .withBackgroundColor(.blue)
+            .withPositionConstraint(from: .top, to: .bottom, of: firstView, offset: 20)
+            .withPositionConstraint(to: .centerX, of: firstView)
+            .withSizeConstraint(.height, value: 30)
+            .withSizeConstraint(.width, value: 80)
     }
 
     override func didReceiveMemoryWarning() {
